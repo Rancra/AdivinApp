3b717867b99cd0554ebd604390faa0e2962d5d51
unknown <Rancra>
2019-09-24 10:56:43 +0100

    second commit

--- src/main/java/dad/javafx/adivinapp/AdivinApp.java
+++ src/main/java/dad/javafx/adivinapp/AdivinApp.java
@@ -13,7 +13,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class AdivinApp extends Application {
-	
+
 	private TextField nombreText;
 	private Button saludarButton;
 	private Label saludoLabel;
@@ -23,67 +23,94 @@ public class AdivinApp extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+
 		nombreText = new TextField();
 		nombreText.setPromptText("0");
 		nombreText.setMaxWidth(150);
-		
+
 		saludoLabel = new Label("Introduce un número del 1 al 100");
-		
+
 		saludarButton = new Button("Comprobar");
 		saludarButton.setDefaultButton(true);
 		saludarButton.setOnAction(evento -> onsaludarButtonAction(evento));
-		
+
 		VBox root = new VBox();
 		root.setSpacing(5);
 		root.setAlignment(Pos.CENTER);
-		root.getChildren().addAll(nombreText, saludarButton, saludoLabel);
-		
+		root.getChildren().addAll(saludoLabel, nombreText, saludarButton);
+
 		Scene scene = new Scene(root, 320, 200);
-		
+
 		primaryStage.setTitle("CheckPalindrome");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
+
 	}
-	
+
 	private void onsaludarButtonAction(ActionEvent evento) {
-		
+
 		String numeroIntroducido = nombreText.getText();
-		
-		if (Integer.parseInt(nombreText.getText()) == numeroRandom) {
-			Alert alert = new Alert(AlertType.INFORMATION);
+
+		if (!esNumero(nombreText.getText())) {
+			Alert alert = new Alert(AlertType.ERROR);
 			alert.setTitle("AdivinApp");
-			alert.setHeaderText("¡Has ganado!");
-			alert.setContentText("Solo has necesitado "+contador+" intentos. Vuelve a jugar y hazlo mejor.");
+			alert.setHeaderText("Error");
+			alert.setContentText("El número introducido no es válido.");
 
 			alert.showAndWait();
-			contador = 0;
-			numeroRandom = (int) (Math.random() * 100) + 1;
-		}
-		else {
-			
-			if (Integer.parseInt(nombreText.getText()) < numeroRandom) {
-				mayoromenor = "mayor";
-			}
-			else {
-				mayoromenor = "menor";
-			}
-			
-			Alert alert = new Alert(AlertType.WARNING);
-			alert.setTitle("AdivinApp");
-			alert.setHeaderText("¡Has fallado!");
-			alert.setContentText("El número a adivinar es "+mayoromenor+" que "+numeroIntroducido+". Vuelve a intentarlo.");
+		} else {
 
-			alert.showAndWait();
-			contador++;
+			if (Integer.parseInt(nombreText.getText()) == numeroRandom) {
+
+				contador++;
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle("AdivinApp");
+				alert.setHeaderText("¡Has ganado!");
+				alert.setContentText(
+						"Solo has necesitado " + contador + " intentos. \n\n Vuelve a jugar y hazlo mejor.");
+
+				alert.showAndWait();
+				contador = 0;
+				numeroRandom = (int) (Math.random() * 100) + 1;
+			} else {
+
+				if (Integer.parseInt(nombreText.getText()) < numeroRandom) {
+					mayoromenor = "mayor";
+				} else {
+					mayoromenor = "menor";
+				}
+
+				Alert alert = new Alert(AlertType.WARNING);
+				alert.setTitle("AdivinApp");
+				alert.setHeaderText("¡Has fallado!");
+				alert.setContentText("El número a adivinar es " + mayoromenor + " que " + numeroIntroducido
+						+ ". \n\n Vuelve a intentarlo.");
+
+				alert.showAndWait();
+				contador++;
+
+			}
+
 		}
-		
-	
+
+	}
+
+	public static boolean esNumero(String cadena) {
+
+		boolean resultado;
+
+		try {
+			Integer.parseInt(cadena);
+			resultado = true;
+		} catch (NumberFormatException excepcion) {
+			resultado = false;
 		}
+
+		return resultado;
+	}
 
 	public static void main(String[] args) {
-		
+
 		launch(args);
 
 	}
